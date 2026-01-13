bca99e40373af5573fcea29b6fd68ee3131a70d8
davsta10 <96589861+davidsousa10@users.noreply.github.com>
2026-01-14 00:16:08 +0100

    Initial Commit

---
+++ 42/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  42
+//
+//  Created by David Sousa Orta on 14/1/26.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return EXIT_SUCCESS;
+}
