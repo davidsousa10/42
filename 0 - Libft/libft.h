2045bce876d7a7341c69ce5e04b817adafcc6ab5
David Sousa <david.sousaorta.com>
2026-01-14 20:42:52 +0100

    varios ft

--- 0 - Libft/libft.h
+++ 0 - Libft/libft.h
@@ -5,5 +5,7 @@
 # include <stdlib.h>
 
 int	ft_isalpha(int c);	
+int ft_isdigit(int c);
+int ft_isalnum(int c);
 
 #endif
--- 0 - Libft/main.c
+++ 0 - Libft/main.c
@@ -3,7 +3,24 @@
 
 int main(void)
 {
-    printf("%zu\n", ft_strlen("hola"));
-    printf("%zu\n", ft_strlen(""));
+    char tests[] = {'A', 'z', '0', '5', '9', 'a', '-', ' ', '\n'};
+    int i = 0;
+
+    printf(" char | isalpha | isdigit | isalnum\n");
+    printf("------+---------+----------+---------\n");
+
+    while (i < 9)
+    {
+        if (tests[i] == '\n')
+            printf("\\n   |");
+        else
+            printf(" '%c'  |", tests[i]);
+
+        printf("    %d    |    %d     |    %d\n",
+               ft_isalpha(tests[i]),
+               ft_isdigit(tests[i]),
+               ft_isalnum(tests[i]));
+        i++;
+    }
     return (0);
 }
