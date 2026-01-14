639c8ea37716ece4b2b9eae73182ea86de970fa4
David Sousa <david.sousaorta.com>
2026-01-14 21:34:42 +0100

    subida

--- 0 - Libft/libft.h
+++ 0 - Libft/libft.h
@@ -1,11 +1,21 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 
-# include <unistd.h>
 # include <stdlib.h>
+# include <unistd.h>
 
-int	ft_isalpha(int c);	
-int ft_isdigit(int c);
-int ft_isalnum(int c);
+/* ===== Character checks ===== */
+int     ft_isalpha(int c);
+int     ft_isdigit(int c);
+int     ft_isalnum(int c);
+int     ft_isascii(int c);
+int     ft_isprint(int c);
+
+/* ===== String functions ===== */
+size_t  ft_strlen(const char *s);
+
+/* ===== Memory functions ===== */
+void    *ft_memset(void *b, int c, size_t len);
+void    ft_bzero(void *s, size_t n);
 
 #endif
--- 0 - Libft/main.c
+++ 0 - Libft/main.c
@@ -1,26 +1,71 @@
 #include <stdio.h>
 #include "libft.h"
 
-int main(void)
+void    test_ctype(void)
 {
-    char tests[] = {'A', 'z', '0', '5', '9', 'a', '-', ' ', '\n'};
+    char tests[] = {'A', 'z', '0', '9', 'a', '-', ' ', '\n', 127, 128};
     int i = 0;
 
-    printf(" char | isalpha | isdigit | isalnum\n");
-    printf("------+---------+----------+---------\n");
+    printf("=== CTYPE TESTS ===\n");
+    printf(" char | isalpha | isdigit | isalnum | isascii | isprint\n");
+    printf("------+---------+----------+---------+----------+---------\n");
 
-    while (i < 9)
+    while (i < 10)
     {
         if (tests[i] == '\n')
             printf("\\n   |");
+        else if (tests[i] < 32 || tests[i] > 126)
+            printf("%3d  |", tests[i]);
         else
             printf(" '%c'  |", tests[i]);
 
-        printf("    %d    |    %d     |    %d\n",
+        printf("    %d    |    %d     |    %d    |    %d     |    %d\n",
                ft_isalpha(tests[i]),
                ft_isdigit(tests[i]),
-               ft_isalnum(tests[i]));
+               ft_isalnum(tests[i]),
+               ft_isascii(tests[i]),
+               ft_isprint(tests[i]));
         i++;
     }
+    printf("\n");
+}
+
+void    test_strlen(void)
+{
+    printf("=== STRLEN TESTS ===\n");
+    printf("\"hola\"  -> %zu\n", ft_strlen("hola"));
+    printf("\"\"      -> %zu\n", ft_strlen(""));
+    printf("\"libft\" -> %zu\n", ft_strlen("libft"));
+    printf("\n");
+}
+
+void    test_memset(void)
+{
+    char s[10] = "abcdefghi";
+
+    printf("=== MEMSET TESTS ===\n");
+    printf("before: %s\n", s);
+    ft_memset(s, 'X', 4);
+    printf("after : %s\n", s);
+    printf("\n");
+}
+
+void    test_bzero(void)
+{
+    char s[10] = "abcdefghi";
+
+    printf("=== BZERO TESTS ===\n");
+    printf("before: %s\n", s);
+    ft_bzero(s, 5);
+    printf("after : %s\n", s);
+    printf("\n");
+}
+
+int main(void)
+{
+    test_ctype();
+    test_strlen();
+    test_memset();
+    test_bzero();
     return (0);
 }
